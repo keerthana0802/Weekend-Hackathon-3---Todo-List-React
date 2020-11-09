d534f79eb812cb1748acafea0e7d2ef429c97993
keerthana0802 <68751743+keerthana0802@users.noreply.github.com>
2020-11-09 20:47:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,78 @@
-import React, {useState, useRef} from "react";
-import Task from "./task";
+import React from "react";
 import "./../styles/App.css";
+import ListView from "./ListView";
 
-function App() {
-  const [tasks, setTasks] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-
-  const taskId = useRef(0);
-
+export default function App() {
+  const [newItem, setNewItem] = React.useState("");
+  const [editIndex, setEditIndex] = React.useState("-1");
+  const [editItem, setEditItem] = React.useState("");
+  const [list, setList] = React.useState([]);
+  const addToDo = () => {
+    let newToDo = newItem;
+    if (newToDo === "") {
+      return;
+    }
+    let presentTask = list.map(item => item.task);
+    if (presentTask.includes(newToDo)) {
+      setEditItem("");
+      return;
+    }
+    let newToDoObj = {
+      task: newToDo,
+      edit: false
+    }
+    let newList = [...list, newToDoObj];
+    setList(newList);
+    setNewItem("");
+  };
   const handleChange = (event) => {
-    setInputValue(event.target.value);
+    setNewItem(event.target.value);
   };
-
-  const handleClick = () => {
-    const updatedTasks = [...tasks];
-    updatedTasks.push({id: taskId.current, text: inputValue});
-    taskId.current = taskId.current + 1;
-    setInputValue("");
-    setTasks(updatedTasks);
+  const handleEditChange = (event) => {
+    setEditItem(event.target.value);
   };
-
-  const handleDelete = (id) => {
-    const taskCopy = [...tasks];
-    const filteredTasks = taskCopy.filter((task) => (task.id !== id ? task : null));
-    setTasks(filteredTasks);
+  const saveEditToDo = () => {
+    let listToEdit = [...list];
+    listToEdit[editIndex].task = editItem;
+    listToEdit[editIndex].edit = false;
+    setList(listToEdit);
+    setEditIndex(-1);
+    setEditItem("");
   };
-
-  const saveChangedText = (id, newtext) => {
-    const taskCopy = [...tasks];
-    taskCopy.forEach((task) => {
-      if (task.id === id) {
-        task.text = newtext;
-      }
-    });
-    setTasks(taskCopy);
+  const handleDelete = (index) => {
+    let listToUpdate = [...list];
+    listToUpdate.splice(index, 1);
+    setList(listToUpdate);
   };
-
+  
+  const handleEdit = (index) => {
+    let editObj = list[index];
+    editObj.edit = true;
+    let newEditList = [...list];
+    newEditList[index] = editObj;
+    setEditIndex(index);
+    setList(newEditList);
+  };
   return (
     <div id="main">
-      <ul>
-        {tasks.map((task) => (
-          <Task key={task.id} task={task} saveChangedText={saveChangedText} handleDelete={handleDelete} />
-        ))}
-      </ul>
-
-      <input id="task" type="textarea" value={inputValue} onChange={handleChange}></input>
-      <button id="btn" onClick={handleClick} disabled={!inputValue}>
-        Add Task
+      <input type="string" id="task" onChange={handleChange} value={newItem} />
+      <button style={{ margin: "5px" }} id="btn" onClick={addToDo}>
+        Add
       </button>
+      <br />
+      {list.map((item, index) => (
+        <ListView
+          task={item.task}
+          edit={item.edit}
+          key={index}
+          serial={index}
+          editItem={editItem}
+          handleEditChange={handleEditChange}
+          saveEditToDo={saveEditToDo}
+          handleDelete={() => handleDelete(index)}
+          handleEdit={() => handleEdit(index)}
+        />
+      ))}
     </div>
   );
 }
-
-export default App;
